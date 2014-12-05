46d584f095ae6f3dfc642e977996fcae2715aecf
Mac Gaulin <gaulinmp+git@gmail.com>
2014-12-04 21:03:08 -0600

    Fixed internal IP code to not get 127

--- send_ip.py
+++ send_ip.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import ipgetter
 import socket
 try:
@@ -5,9 +7,14 @@ try:
 except:
     from urllib.request import urlopen
 
+def get_internal_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('google.com', 0))
+    return s.getsockname()[0]
+
 def url_string():
     comp_name = socket.gethostname().split(".")[0]
-    ip_internal = socket.gethostbyname(socket.gethostname())
+    ip_internal = get_internal_ip()
     ip_external = ipgetter.myip()
     return ("comp_name={}_internal&comp_ip={}".format(comp_name, ip_internal),
             "comp_name={}_external&comp_ip={}".format(comp_name, ip_external),)
