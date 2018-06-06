52a5d7ab13faf887462e0d52b26c92cd7bb7b1f8
Mac Gaulin <gaulinmp+git@gmail.com>
2018-06-06 00:26:58 -0600

    Make internal IP scrape work on Mac

--- send_ip.py
+++ send_ip.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
+import re
 import sh
 import json
 import requests
 import socket
-try:
-    from urllib2 import urlopen
-except:
-    from urllib.request import urlopen
     
 PASSWORD_FILE = 'macs_ips.password'
 SERVER_ROOT = 'http://mgaulin.com/projects/macs_ips'
@@ -18,18 +15,27 @@ def get_internal_ip():
     return s.getsockname()[0]
 
 def url_string():
+    _r = []
     comp_name = socket.gethostname().split(".")[0]
-    ip_internal = [x.split('inet addr:')[-1].split()[0] for x in sh.ifconfig().split("\n\n") if x.startswith('e')][0]
+
+    for eth in re.compile(r'\n\b', re.I).split(str(sh.ifconfig())):
+        if not eth or eth[0] != 'e':
+            continue
+        ip = re.findall(r'\binet [^\d]*((?:\d{1,3}\.){3}\d{1,3})', eth, re.I)
+        iname = re.findall(r'^[a-z0-9]*', eth, re.I)
+        if ip and iname:
+            _r.append("comp_name={}_{}&comp_ip={}"
+                      .format(comp_name, iname[0], ip[0]))
+
+    #ip_internal = [x.split('inet addr:')[-1].split()[0] for x in sh.ifconfig().split("\n\n") if x.startswith('e')][0]
     try:
         resp = requests.get("https://api.ipify.org?format=json")
         ip_external = json.loads(resp.content.decode('utf-8'))['ip']
+        if ip_external:
+            _r.append("comp_name={}_external&comp_ip={}".format(comp_name, ip_external))
     except:
-        ip_external = None
-
-    _r = ["comp_name={}_internal&comp_ip={}".format(comp_name, ip_internal), ]
-    if ip_external:
-        _r.append("comp_name={}_external&comp_ip={}".format(comp_name, ip_external))
-
+        pass
+    print(_r)
     return _r
 
 if __name__ == "__main__":
@@ -38,4 +44,4 @@ if __name__ == "__main__":
     for url in url_string():
         full_url = ("{}/secret_write?secret_word={}&{}"
                     .format(SERVER_ROOT, pword, url))
-        urlopen(full_url)
+        print(requests.get(full_url).content)
